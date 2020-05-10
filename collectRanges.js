2cb0f35c3da10eb3ae33be7003ab17d4f3794077
jabrayare <jabra5980@gmail.com>
2020-05-10 14:10:13 -0400

    added 05/10/2020

--- collectRanges.js
+++ collectRanges.js
@@ -1,15 +1,21 @@
 let collectRanges = function(num){
   let begin = 0;
-  let arr = []
+  let result = []
   for(let i = 0; i < num.length; i++){
       if(num[i+1] - num[i] === 1){
         continue;
-      }else{
-        arr.push(num[]begin+"->"+num[index])
       }
-      index++;
+      if(begin === i){
+        result.push(num[i].toString());
+      }
+      else{
+        result.push(num[begin]+"->"+num[i]) ; 
+      }
+      begin = i +1;
+      console.log(begin);
+
   }
-  return arr;
+  return result;
 }
 
 console.log(collectRanges([0,1,2,4,5,6,7,9]))
